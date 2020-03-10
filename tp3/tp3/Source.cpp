6c4a3ff9e5ca93ae9e57cd953ae19b12e2d8133e
monkin77 <monkinsmurf@gmail.com>
2020-03-10 20:32:45 +0000

    tp3 update

--- tp3/tp3/Source.cpp
+++ tp3/tp3/Source.cpp
@@ -16,11 +16,36 @@ double area(double x1, double y1, double x2, double y2, double x3, double y3) {
 	return area;
 }
 
+double squareRoot(double number, double delta, double maxIter) {
+	double rq = 1;
+	int n = 1;
+	double rqn;
+	double diff = abs( number - pow(rq, 2) ) ;
+	while (diff > delta && n < maxIter) {
+		rqn = ((rq + number) / rq) / 2;
+		rq = rqn;
+		diff = abs(number - pow(rq, 2)) ;
+		n++;
+		cout << rqn << endl;
+}
+	return rqn;
+}
 
 int main() {
+	/* ex 1
 	double x1, y1, x2, y2, x3, y3;
 	cout << "Insert: x1 y1 x2 y2 x3 y3" << endl;
 	cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
 	double result = area(x1, y1, x2, y2, x3, y3);
 	cout << result << endl;
+	*/
+	double number, delta, maxIter;
+	cout << "Insert: number delta maxIter" << endl;
+	cin >> number >> delta >> maxIter;
+	double result = squareRoot(number, delta, maxIter);
+	cout << result << endl;
+
+
+
+	return 0;
 }
